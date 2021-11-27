3c4e7142144fbb0820f5844a4a635ff78a25932c
Rhea M <rheamalviya@rheas-air-2.myfiosgateway.com>
2021-11-27 11:09:08 -0500

    window and console object functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!")
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  var playerName=window.prompt("What's your robot's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, very good for sending yourself a message");
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("The robot's name is" + playerName);
+ 
+
+// fight();
